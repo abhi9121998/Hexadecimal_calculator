fdec1db6e67634efe0e68eb867ee2493154bb0f3
abhi9121998 <38099265+abhi9121998@users.noreply.github.com>
2018-10-09 17:05:44 +0530

    Add files via upload
    multiplication update

--- multiply.js
+++ multiply.js
@@ -5,30 +5,35 @@ function mul(a,b)
 {
     var x=parseInt(getValue1(a));
     var y=parseInt(getValue1(b));
+    //alert('carry : '+carry);
     var value=x*y+carry;
+    //alert('value : '+value);
     if(value>15)
     {
-        value=value%16;
         carry=Math.floor(value/16);
+        value=value%16;
     }
+    else
+        carry=0;
     value=check(value);
-    alert('value : '+value);
     return value;
 }
 
 function multiplication(input_array)
 {
-    var a,b;
+    var a='',b='';
     var i,j;
-    if(input_array[0]>=input_array[1])
+    if(input_array[0].length>=input_array[1].length)
     {
-        a=input_array[0],b=input_array[1];
+        a=input_array[0];
+        b=input_array[1];
         i=a.length-1;
         j=b.length-1;
     }
     else
     {
-        a=input_array[1],b=input_array[0];
+        a=input_array[1];
+        b=input_array[0];
         i=a.length-1;
         j=b.length-1;
     }
@@ -40,16 +45,24 @@ function multiplication(input_array)
         var temp=i;
         for(i ; i>=0; i--)
         {
-            result[index]=mul(a[j],b[i])+result[index];
-            alert(result[index]);
+            result[index]=mul(a[i],b[j])+result[index];
         }
         if(carry!==0)
         {
             result[index]=carry+result[index];
         }
+        for(var k=0; k<index; k++)
+        {
+            result[index]=result[index]+'0';
+        }
         result[index]=result[index].slice(0,result[index].length-1);
-        alert(result[index]);
+        //alert(result[index]);
         i=temp;
         index++;
+        carry=0;
+    }
+    for(var i=0; i<result.length; i++)
+    {
+        alert(result[k]);
     }
 }
